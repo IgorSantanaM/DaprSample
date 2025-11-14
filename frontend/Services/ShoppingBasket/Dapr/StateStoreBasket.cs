8ec8729a3c3b9c2a878fe44b67f0c69032e7ff42
Igor Santana Medeiros <1434@ucdb.br>
2025-11-14 15:18:41 -0400

    (feat): Added Store Management Build Block

---
+++ frontend/Services/ShoppingBasket/Dapr/StateStoreBasket.cs
@@ -0,0 +1,11 @@
+﻿using GloboTicket.Frontend.Models.Api;
+
+namespace GloboTicket.Frontend.Services.ShoppingBasket.Dapr
+{
+    public class StateStoreBasket
+    {
+        public Guid BasketId { get; set; }
+        public List<BasketLine> Lines { get; set; } = new List<BasketLine>();
+        public Guid UserId { get; set; }
+    }
+}
--- ordering/Program.cs
+++ ordering/Program.cs
@@ -8,17 +8,9 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<EmailSender>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseRouting();
 
