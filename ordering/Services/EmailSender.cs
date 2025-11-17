3f496a221cd36fbf0b0d568ac496a4ce50c5a324
Igor Santana Medeiros <1434@ucdb.br>
2025-11-17 15:47:30 -0400

    (feat): Added bindings and observability

--- ordering/Services/EmailSender.cs
+++ ordering/Services/EmailSender.cs
@@ -1,20 +1,32 @@
+using Dapr.Client;
 using GloboTicket.Ordering.Model;
 
 namespace GloboTicket.Ordering.Services;
 
 public class EmailSender
 {
+    private readonly DaprClient daprClient;
     private readonly ILogger<EmailSender> logger;
 
-    public EmailSender(ILogger<EmailSender> logger)
+    public EmailSender(ILogger<EmailSender> logger, DaprClient daprClient)
     {
         this.logger = logger;
+        this.daprClient = daprClient;
     }
 
     public async Task SendEmailForOrder(OrderForCreation order)
     {
         logger.LogInformation($"Received a new order for {order.CustomerDetails.Email}");
         logger.LogWarning("Not using Dapr so no email sent");
+        var metadata = new Dictionary<string, string>
+        {
+            ["emailFrom"] = "noreply@globoticket.shop",
+            ["emailTo"] = order.CustomerDetails.Email,
+            ["emailSubject"] = "Order Confirmation"
+        };
+        var body = $"<h2>Your order has been received</h2>"
+            + "<p>Your tickets are on the way!</p>";
+        await daprClient.InvokeBindingAsync("sendmail", "create", body, metadata);
     }
 }
 
