82a40922b1abd15fb63b98d4d48d30ad726f9450
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-11-16 21:30:40 -0400

    (fix): Fixed PUBSUB

--- ordering/Program.cs
+++ ordering/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddDapr();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -13,11 +13,13 @@ builder.Services.AddTransient<EmailSender>();
 var app = builder.Build();
 
 app.UseRouting();
+app.UseCloudEvents();
 
 app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
+    endpoints.MapSubscribeHandler();
     endpoints.MapControllers();
 });
 
